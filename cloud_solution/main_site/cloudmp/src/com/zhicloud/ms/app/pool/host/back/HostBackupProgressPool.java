4cf87bc539d80c681de5f38756efe6315df820a9
wulisha <wulisha@zhicloud.com>
2015-12-08 15:52:46 +0800

    删除调试日志

--- cloud_solution/main_site/cloudmp/src/com/zhicloud/ms/app/pool/host/back/HostBackupProgressPool.java
+++ cloud_solution/main_site/cloudmp/src/com/zhicloud/ms/app/pool/host/back/HostBackupProgressPool.java
@@ -3,8 +3,6 @@ package com.zhicloud.ms.app.pool.host.back;
 import java.util.Hashtable;
 import java.util.Map;
 
-import com.sun.media.jfxmedia.logging.Logger;
-
 public class HostBackupProgressPool {
 
 	private Map<String, HostBackupProgressData> pool;
@@ -23,8 +21,7 @@ public class HostBackupProgressPool {
 		if (uuid != null && uuid.trim().length() != 0 ) {
 			String key = uuid;
 			pool.put(key, hostBackup);
-		}
-	    System.out.println("hhahhahahhahahhahahahah put"+ uuid);
+		} 
 
 	}
 
@@ -43,8 +40,7 @@ public class HostBackupProgressPool {
 		if (sessionId != null && sessionId.trim().length() != 0 ) {
 			String key = sessionId;
 			pool.remove(key);
-		}
-		System.out.println("hhahhahahhahahhahahahah remove"+ sessionId);
+		} 
 	}
 	
 //	public HostBackupProgressData getDuplication(String sessionId) {
