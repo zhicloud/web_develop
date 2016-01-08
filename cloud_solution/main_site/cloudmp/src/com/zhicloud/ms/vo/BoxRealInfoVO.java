687c2f81c625ad90b37b2f65e339cc14626b76e3
Sean <zhang.xiang1991@gmail.com>
2016-01-08 11:19:57 +0800

    Fixed 累计在线时长重置问题
    修复盒子再次登录累计在线时长重置问题

--- cloud_solution/main_site/cloudmp/src/com/zhicloud/ms/vo/BoxRealInfoVO.java
+++ cloud_solution/main_site/cloudmp/src/com/zhicloud/ms/vo/BoxRealInfoVO.java
@@ -33,7 +33,7 @@ public class BoxRealInfoVO {
     private Date lastLogoutDate;
     private String serialNumber;        //盒子序列号
     
-    private long cumulativeOnlineTime;
+    private long cumulativeOnlineTime;  //累计在线时间
 
     public long getCumulativeOnlineTime() {
         return cumulativeOnlineTime;
